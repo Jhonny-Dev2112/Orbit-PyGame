16cbf3fd370bac951d0bb84a20a00ba925389fb4
Jhonatan Fernandes Santana <jhonny.dev2112@gmail.com>
2026-06-19 15:27:59 -0300

    Creating asset folder Inserting all assets Creating the menu: backdroung, sound and fonts Creating the const files

---
+++ code/Const.py
@@ -0,0 +1,13 @@
+# C
+COLOR_WHITE = (255,255,255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
