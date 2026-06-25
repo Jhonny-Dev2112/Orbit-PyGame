b7e7dd52ebaab54f187cb8cbfc0be0784f26b2bc
Jhonatan Fernandes Santana <jhonny.dev2112@gmail.com>
2026-06-25 18:22:55 -0300

    Creating Players and Enemies Moving Players and Enemies Add assets of player and Enemies

--- code/Const.py
+++ code/Const.py
@@ -1,15 +1,22 @@
 # C
+import pygame
 
 COLOR_WHITE = (255,255,255)
 COLOR_YELLOW = (255,255,0)
 COLOR_BLUE_DARK = (2, 0, 101)
 
 # E
-
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
     'Level1Bg2': 5,
+    'Player1' : 5,
+    'Player2' : 5,
+    'Enemy1': 4,
+    'Enemy2': 9,
+    'Enemy3': 4,
+    'Enemy4': 4,
 }
 
 
@@ -20,6 +27,26 @@ MENU_OPTION = ('New Game',
                'SCORE',
                'EXIT')
 
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
+
+# S
+SPAWN_TIME = 4000
+
 # W
 WIN_WIDTH = 800
 WIN_HEIGHT = 600
+
